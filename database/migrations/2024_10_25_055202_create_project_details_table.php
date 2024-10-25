5b78ef85d69c73605e40736432780213927230a9
F313-056\user <D1114182007@gm.lhu.edu.tw>
2024-10-25 14:05:50 +0800

    改成字串

--- database/migrations/2024_10_25_055202_create_project_details_table.php
+++ database/migrations/2024_10_25_055202_create_project_details_table.php
@@ -20,8 +20,8 @@ class CreateProjectDetailsTable extends Migration
             $table->string('application_number', 20)->comment('送審案件編號');
             $table->string('implementation_area', 20)->comment('實施區域縣市別');
             $table->string('township', 20)->comment('鄉鎮區別');
-            $table->integer('province_city_code')->comment('省市縣市之代碼');
-            $table->integer('province_city_town_code')->comment('省市縣市鄉鎮市區之代碼');
+            $table->string('province_city_code')->comment('省市縣市之代碼');
+            $table->string('province_city_town_code')->comment('省市縣市鄉鎮市區之代碼');
             $table->string('project_name', 255)->comment('工程名稱');
             $table->integer('central_grant')->comment('中央補助款');
             $table->integer('local_matching_funds')->comment('地方配合款');
